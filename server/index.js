7c51ec9d93ef122c87cc13a5dc0898b0d57deef7
Susy(Qinqin) Yang <susyyang24@gmail.com>
2020-05-30 20:52:56 -0400

    get ready to dockerize

---
+++ server/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3555;
+
+const { Pool } = require('pg');
+const pool = new Pool({
+    host: 'qnapostgres',
+    user: 'postgres',
+    password: '123456789',
+    database: 'postgres',
+    port: 5432,
+    max: 1000,
+    idleTimeoutMillis: 3000,
+    connectionTimeoutMillis: 3000,
+    maxUses: 7500,
+});
+
+
+app.get("/", (req, res) => {
+    let queryStr = 'select * from questions_transformed where id = 1';
+    pool
+        .connect()
+        .then((client) => {
+            return client
+            .query(queryStr)
+            .then((dbRes) => {
+                client.release();
+                res.status(200).json(dbRes);
+            })
+            .catch((err) => {
+                client.release();
+                console.log(`Get server err: ${err}`);
+                res.status(400);
+            })
+        })
+});
+
+pool.end().then(() => console.log('pg pool has ended'))
+
+app.listen(port, () => {
+  console.log(`Server is listening on port: ${port}`);
+});
