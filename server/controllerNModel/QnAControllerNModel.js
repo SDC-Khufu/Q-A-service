f8d9e4e9cc0c9caa5bc653e33d862c18a471165d
Susy(Qinqin) Yang <susyyang24@gmail.com>
2020-06-03 16:46:26 -0400

    completed all routes with 3 tables

--- server/controllerNModel/QnAControllerNModel.js
+++ server/controllerNModel/QnAControllerNModel.js
@@ -6,9 +6,10 @@ const transform = (dbRes, product_id) => {
   let curQId = 0;
   if (dbRes.rows.length) {
     dbRes.rows.forEach((row, ind) => {
-      if (ind === dbRes.rows.length - 1) {
-        finalData.results = finalData.results.concat(tempQnA);
-      };
+      let photoUrl = row.url !== null ? [row.url] : []
+      if (row.url !== null && row.url[0] === '[') {
+        photoUrl = JSON.parse(row.url);
+      }
       if (row.question_id !== curQId) {
         if (curQId !== 0) { finalData.results = finalData.results.concat(tempQnA) };
         curQId = row.question_id;
@@ -28,7 +29,8 @@ const transform = (dbRes, product_id) => {
             "date": row.date,
             "answerer_name": row.answerer_name,
             "helpfulness": row.helpfulness,
-            "photos": row.url !== null ? [row.url] : []
+            // "photos": row.url !== null ? [row.url] : []
+            "photos": photoUrl
           }
         }
       } else {
@@ -41,10 +43,13 @@ const transform = (dbRes, product_id) => {
             "date": row.date,
             "answerer_name": row.answerer_name,
             "helpfulness": row.helpfulness,
-            "photos": row.url !== null ? [row.url] : []
+            "photos": photoUrl
           }
         }
       }
+      if (ind === dbRes.rows.length - 1) {
+        finalData.results = finalData.results.concat(tempQnA);
+      };
     })
   }
   return finalData;
@@ -61,6 +66,7 @@ const getQuestionsByProductId = async (req, res) => {
   const client = await pool.connect();
   try {
     const dbRes = await client.query(queryStr);
+    // console.log('dbRes.rows', dbRes.rows)
     let transformedRes = transform(dbRes, product_id);
     res.status(200).json(transformedRes);
   } catch (err) {
@@ -73,24 +79,24 @@ const getQuestionsByProductId = async (req, res) => {
 
 //ignore this one for now
 const getAnswersByQuestionId = async (req, res) => {
-  //   console.log("req params for A", req.params);
-  //   const question_id = req.params.question_id;
-  //   let queryStr = `select * from answers_transformed where question_id = ${question_id}`;
-  //   const client = await pool.connect();
-  //   try {
-  //     const dbRes = await client.query(queryStr);
-  //     res.status(200).json(dbRes.rows);
-  //   } catch (err) {
-  //     console.log("Get server err for getting answers: ", err);
-  //     res.status(400).json(`Couldn't get answers for this question`);
-  //   } finally {
-  //     await client.release();
-  //   }
+    // console.log("req params for A", req.params);
+    // const question_id = req.params.question_id;
+    // let queryStr = `select * from raw_answers where question_id = ${question_id}`;
+    // const client = await pool.connect();
+    // try {
+    //   const dbRes = await client.query(queryStr);
+    //   res.status(200).json(dbRes.rows);
+    // } catch (err) {
+    //   console.log("Get server err for getting answers: ", err);
+    //   res.status(400).json(`Couldn't get answers for this question`);
+    // } finally {
+    //   await client.release();
+    // }
 };
 
 const addQuestionToAProductId = async (req, res) => {
   console.log('pramas', req.params, 'body', req.body);
-  let product_id = req.params.product_id;
+  let product_id = Number(req.params.product_id);
   let queryStr = `insert into questions_transformed (product_id, question_body, question_date, asker_name, asker_email) 
                   values ($1, $2, $3, $4, $5);`
   let postDate = new Date();
@@ -98,7 +104,7 @@ const addQuestionToAProductId = async (req, res) => {
   const client = await pool.connect();
   try {
     const dbRes = await client.query(queryStr, values);
-    res.status(201).json(dbRes);
+    res.status(201).json('Created');
   } catch (err) {
     console.log("Get server err for posting questions: ", err);
     res.status(500).json(`Couldn't post question for this product`);
@@ -107,35 +113,102 @@ const addQuestionToAProductId = async (req, res) => {
   }
 };
 
-// const getAnswersByQuestionId = (question_id) => {
-//   const fromDate = new Date();
-//   let queryStr = `select * from answers_transformed where id = ${question_id}`;
-//   pool.connect().then((client) => {
-//     return client
-//       .query(queryStr)
-//       .then((dbRes) => {
-//         client.release();
-//         console.log(`Only go through for successful getAnswersByQuestionId`);
-//         const toDate = new Date();
-//         const elapsed = toDate.getTime() - fromDate.getTime();
-//         let answersWithTime = [{ elapsed }, dbRes.rows];
-//         console.log('answersWithTime', answersWithTime);
-//         return answersWithTime;
-//       })
-//       .catch((err) => {
-//         client.release();
-//         console.log(`Get server err for getAnswersByQuestionId: ${err}`);
-//       });
-//   });
-// };
+const addAnswerToAQuestionId = async (req, res) => {
+  console.log("req params for Add Answer", req.params, 'body for add answer', req.body);
+  let question_id = Number(req.params.question_id);
+  let answerQueryStr = `insert into answers_transformed (question_id, body, date, answerer_name, answerer_email) 
+                  values ($1, $2, $3, $4, $5) returning answer_id;`
+  let postDate = new Date();
+  let answerValues = [question_id, req.body.body, postDate, req.body.name, req.body.email];
+  const client = await pool.connect();
+  try {
+    const raw_answer_id = await client.query(answerQueryStr, answerValues);
+    if (req.body.photos.length > 0) {
+      let answer_id = Number(raw_answer_id.rows[0].answer_id);
+      let photoQueryStr = `insert into answers_photos_final (answer_id, url) 
+      values ($1, $2);`
+      let photoValues = [answer_id, JSON.stringify(req.body.photos)];
+      const dbRes = await client.query(photoQueryStr, photoValues);
+    }
+    res.status(201).json('Created');
+  } catch (err) {
+    console.log("Get server err for posting answers: ", err);
+    res.status(500).json(`Couldn't post answer for this question`);
+  } finally {
+    await client.release();
+  }
+}
 
+const markHelpfulByQuestionId = async (req, res) => {
+  let question_id = Number(req.params.question_id);
+  let queryStr = `update questions_transformed set question_helpfulness = question_helpfulness + 1 
+                  where question_id = ${question_id}`;
+  const client = await pool.connect();
+  try {
+    const dbRes = await client.query(queryStr);
+    res.status(204).json('');
+  } catch (err) {
+    console.log("Get server err for adding helpful: ", err);
+    res.status(500).json(`Couldn't add helpful for this question`);
+  } finally {
+    await client.release();
+  }
+};
+
+const markHelpfulByAnswerId = async (req, res) => {
+  let answer_id = Number(req.params.answer_id);
+  let queryStr = `update answers_transformed set helpfulness = helpfulness + 1 
+                  where answer_id = ${answer_id}`;
+  const client = await pool.connect();
+  try {
+    const dbRes = await client.query(queryStr);
+    res.status(204).json('');
+  } catch (err) {
+    console.log("Get server err for adding helpful: ", err);
+    res.status(500).json(`Couldn't add helpful for this answer`);
+  } finally {
+    await client.release();
+  }
+};
+
+const reportByQuestionId = async (req, res) => {
+  let question_id = Number(req.params.question_id);
+  let queryStr = `update questions_transformed set reported = reported + 1 
+                  where question_id = ${question_id}`;
+  const client = await pool.connect();
+  try {
+    const dbRes = await client.query(queryStr);
+    res.status(204).json('');
+  } catch (err) {
+    console.log("Get server err for reporting question: ", err);
+    res.status(500).json(`Couldn't add report this question`);
+  } finally {
+    await client.release();
+  }
+};
+
+const reportByAnswerId = async (req, res) => {
+  let answer_id = Number(req.params.answer_id);
+  let queryStr = `update answers_transformed set reported = reported + 1 
+                  where answer_id = ${answer_id}`;
+  const client = await pool.connect();
+  try {
+    const dbRes = await client.query(queryStr);
+    res.status(204).json('');
+  } catch (err) {
+    console.log("Get server err for reporting answer: ", err);
+    res.status(500).json(`Couldn't add reported for this answer`);
+  } finally {
+    await client.release();
+  }
+};
 module.exports = {
   getQuestionsByProductId,
   getAnswersByQuestionId,
   addQuestionToAProductId,
-  // addAnswerToAQuestionId,
-  // markHelpfulByQuestionId,
-  // markHelpfulByAnswerId,
-  // reportByQuestionId,
-  // reportByAnswerId
+  addAnswerToAQuestionId,
+  markHelpfulByQuestionId,
+  markHelpfulByAnswerId,
+  reportByQuestionId,
+  reportByAnswerId
 };
--- server/router/QnARoutes.js
+++ server/router/QnARoutes.js
@@ -5,11 +5,11 @@ const controller = require('../controllerNModel/QnAControllerNModel');
 router.get('/qa/:product_id', controller.getQuestionsByProductId);
 router.get('/qa/:question_id/answers', controller.getAnswersByQuestionId);
 router.post('/qa/:product_id', controller.addQuestionToAProductId);
-// router.post('/qa/:question_id/answers', controller.addAnswerToAQuestionId);
-// router.put('/qa/question/:question_id/helpful', controller.markHelpfulByQuestionId);
-// router.put('/qa/answer/:answer_id/helpful', controller.markHelpfulByAnswerId);
-// router.put('/qa/question/:question_id/report', controller.reportByQuestionId);
-// router.put('/qa/answer/:answer_id/report', controller.reportByAnswerId);
+router.post('/qa/:question_id/answers', controller.addAnswerToAQuestionId);
+router.put('/qa/question/:question_id/helpful', controller.markHelpfulByQuestionId);
+router.put('/qa/answer/:answer_id/helpful', controller.markHelpfulByAnswerId);
+router.put('/qa/question/:question_id/report', controller.reportByQuestionId);
+router.put('/qa/answer/:answer_id/report', controller.reportByAnswerId);
 
 module.exports = router;
 
